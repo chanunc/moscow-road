6d22bd0d34431fd063cd1883913b27218444a89c
Timothy Lim <timothy_lim@outlook.com>
2016-06-25 19:04:39 +0100

    Finish first batch of tests

--- custom-contract/environments/test/contracts/Example.sol.js
+++ custom-contract/environments/test/contracts/Example.sol.js
@@ -8,7 +8,7 @@
     abi: [{"constant":true,"inputs":[],"name":"value","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"val","type":"uint256"}],"name":"setValue","outputs":[],"type":"function"},{"inputs":[],"type":"constructor"}],
     binary: "60606040526000805560408060146000396000f3606060405260e060020a60003504633fa4f245811460245780635524107714602c575b005b603660005481565b6004356000556022565b6060908152602090f3",
     unlinked_binary: "60606040526000805560408060146000396000f3606060405260e060020a60003504633fa4f245811460245780635524107714602c575b005b603660005481565b6004356000556022565b6060908152602090f3",
-    address: "0xe1ec91f2e7b1484963a5b2dcc5dd9ad96db96bb3",
+    address: "0xc7e23119e8a5ac7fbf30d58a25d4db8369b7089b",
     generated_with: "2.0.9",
     contract_name: "Example"
   };
--- custom-contract/test/crowdfund.js
+++ custom-contract/test/crowdfund.js
@@ -3,9 +3,6 @@ contract('Crowdfund', function(accounts) {
 
     var crowdfund = Crowdfund.at(Crowdfund.deployed_address);
     var repo_owner = accounts[1];
-    var funder1 = accounts[2];
-    var funder2 = accounts[3];
-    var recepient = accounts[4];
 
     // Check the repository owner's address matches the stored address
     Crowdfund.new(repo_owner).then(
@@ -13,21 +10,125 @@ contract('Crowdfund', function(accounts) {
       crowdfund.repo_owner.call().then(
         function(repoOwner){
           assert.equal(accounts[1], repoOwner, "Repository owners account not store properly");
-        }).then(
-        function(){
-          web3.eth.sendTransaction({value:1, from:funder1, to:crowdfund.address, gas:1000000});
-        }).then(
-        function(){
-          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), 1, "balance should match1");
           done();
         }).catch(done);
       }).catch(done);
 
+  });
 
+  it("A single funder should be able to send funds", function(done) {
 
+    var crowdfund = Crowdfund.at(Crowdfund.deployed_address);
+    var repo_owner = accounts[1];
+    var funder1 = accounts[2];
 
+    // Check the repository owner's address matches the stored address
+    Crowdfund.new(repo_owner).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder1, to:crowdfund.address, gas:1000000});
+        }).then(
+        function(){
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), 1, "balance should match");
+          done();
+        }).catch(done);
+  });
+
+  it("Two funders should be able to send funds", function(done) {
+
+    var crowdfund = Crowdfund.at(Crowdfund.deployed_address);
+    var repo_owner = accounts[1];
+    var funder1 = accounts[2];
+    var funder2 = accounts[3];
+
+    var initial = web3.eth.getBalance(crowdfund.address).toNumber();
+
+    Crowdfund.new(repo_owner).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder1, to:crowdfund.address, gas:1000000});
+        }).then(
+        function(){
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial+1, "balance should be 1");
+        }).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder2, to:crowdfund.address, gas:1000000});
+        }).then(
+        function(){
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial+2, "balance should be 2");
+          done();
+        }).catch(done);
+  });
+
+  it("Check that a fund with one funder can be cancelled", function(done){
+
+    var crowdfund = Crowdfund.at(Crowdfund.deployed_address);
+    var repo_owner = accounts[1];
+    var funder1 = accounts[2];
+
+    var initial_contract
+
+    Crowdfund.new(repo_owner, { from: funder1}).then(
+
+      function(crowdfund){
+
+      initial_contract = web3.eth.getBalance(crowdfund.address).toNumber();
+
+      crowdfund.greet.call().then(
+        function(greeting){
+          assert.equal(greeting, "works", "Contract should still be active");
+        }).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder1, to:crowdfund.address, gas:1000000});
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial_contract+1 , "Initial contract balance should now be incremented by 1 ether");
+        }).then(
+        function(){
+          return crowdfund.cancel({from: repo_owner});
+        }).then(
+        function(){
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial_contract , "Balance should now be decremented by back to the intial value");
+          done();
+        }).catch(done);
+    });
+  });
+
+  it("Check that a fund with one funders can be cancelled", function(done){
+
+    var crowdfund = Crowdfund.at(Crowdfund.deployed_address);
+    var repo_owner = accounts[1];
+    var funder1 = accounts[2];
+    var funder2 = accounts[3];
+
+    var initial_contract
+
+    Crowdfund.new(repo_owner, { from: funder1}).then(
+
+      function(crowdfund){
+
+      initial_contract = web3.eth.getBalance(crowdfund.address).toNumber();
+
+      crowdfund.greet.call().then(
+        function(greeting){
+          assert.equal(greeting, "works", "Contract should still be active");
+        }).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder1, to:crowdfund.address, gas:1000000});
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial_contract+1 , "Initial contract balance should now be incremented by 1 ether");
+        }).then(
+        function(){
+          web3.eth.sendTransaction({value:1, from:funder2, to:crowdfund.address, gas:1000000});
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial_contract+2 , "Initial contract balance should now be incremented by 1 ether");
+        }).then(
+        function(){
+          return crowdfund.cancel({from: repo_owner});
+        }).then(
+        function(){
+          assert.equal(web3.eth.getBalance(crowdfund.address).toNumber(), initial_contract , "Balance should now be decremented by back to the intial value");
+          done();
+        }).catch(done);
+    });
   });
 });
+
+
 
 // contract('Example', function(accounts){
 //   it("should assert true", function(done) {
